d2d3c9c1b16604db4081b3184481cba07ba3585a
Will Midgette <wamidgette@gmail.com>
2021-12-27 13:58:30 -0500

    homepage styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,8 +9,12 @@ const HomeWrapper = styled.div`
 `;
 const HomeLogo = styled.img`
   border-radius: 50%;
-  max-width: 30%;
-  transform: rotate(-20deg);
+  width: 300px;
+  animation: rotate20 0.7s cubic-bezier(0.68, -0.6, 0.32, 1.6) forwards;
+  cursor:pointer;
+  @media (max-width: 500px){
+    width: 200px;
+  }
 `;
 
 export default function HomePage() {
