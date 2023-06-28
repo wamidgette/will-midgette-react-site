1324f6bb9c5de23a16d54592db10dd695005d698
Will Midgette <71841261+wamidgette@users.noreply.github.com>
2023-06-27 22:05:17 -0400

    update resume and projects

--- src/components/ProjectsArray.js
+++ src/components/ProjectsArray.js
@@ -34,34 +34,34 @@ const ProjectsArray = [
     description:
       "Custom API function for property rental company. Client requested a way to automate changes to their website's list of properties daily. Worked with client's property management company to create API endpoint that receives JSON payloads containing all property data on a daily schedule, and parses it into the client's property object data structure in WordPress.",
   },
-  {
-    image_path: "/images/projects/daredevil-dave.jpg",
-    thumbnail_path: "/images/projects/thumbnails/daredevil-dave.jpg",
-    tags: ["CSS, JavaScript"],
-    title: "Daredevil Dave",
-    description:
-      "Shoot Daredevil Dave out of the cannon and through the hoop with the spacebar. This game uses CSS animations triggered by JavaScript event listeners and features a score tracker and countdown.",
-    projectUrl: "https://wamidgette.github.io/daredevil-dave/",
-    githubUrl: "https://github.com/wamidgette/DaredevilDave",
-  },
-  {
-    image_path: "/images/projects/nodejs.jpg",
-    thumbnail_path: "/images/projects/thumbnails/nodejs.jpg",
-    tags: ["Node.js, Express.js"],
-    title: "Node.js API Coding Challenge",
-    description:
-      "This is a coding challenge I completed using Node and Express. The purpose was to create a queryable API that called upon an external API and rearranged the data returned based on the queries given by the user. Caching is utilized to improve performance.",
-    githubUrl: "https://github.com/wamidgette/nodeApiCodingChallenge",
-  },
-  {
-    image_path: "/images/projects/motorcycle-trips.jpg",
-    thumbnail_path: "/images/projects/thumbnails/motorcycle-trips.jpg",
-    tags: ["MongoDB, Express.js, React.js, Node.js"],
-    title: "Motorcycle Trips React App",
-    description:
-      "This is a full-stack node.js application, and is a work in progress. Motorcycle riders can browse through and sign up for trips with other riders. Admin users can create, update and delete trips, riders, events as they choose. Express.js used to interact with MongoDB database. React.js makes requests to API endpoints on Node.js server to render data.",
-    githubUrl: "https://github.com/wamidgette/MotorcycleRidersMERNApp",
-  },
+  // {
+  //   image_path: "/images/projects/daredevil-dave.jpg",
+  //   thumbnail_path: "/images/projects/thumbnails/daredevil-dave.jpg",
+  //   tags: ["CSS, JavaScript"],
+  //   title: "Daredevil Dave",
+  //   description:
+  //     "Shoot Daredevil Dave out of the cannon and through the hoop with the spacebar. This game uses CSS animations triggered by JavaScript event listeners and features a score tracker and countdown.",
+  //   projectUrl: "https://wamidgette.github.io/daredevil-dave/",
+  //   githubUrl: "https://github.com/wamidgette/DaredevilDave",
+  // },
+  // {
+  //   image_path: "/images/projects/nodejs.jpg",
+  //   thumbnail_path: "/images/projects/thumbnails/nodejs.jpg",
+  //   tags: ["Node.js, Express.js"],
+  //   title: "Node.js API Coding Challenge",
+  //   description:
+  //     "This is a coding challenge I completed using Node and Express. The purpose was to create a queryable API that called upon an external API and rearranged the data returned based on the queries given by the user. Caching is utilized to improve performance.",
+  //   githubUrl: "https://github.com/wamidgette/nodeApiCodingChallenge",
+  // },
+  // {
+  //   image_path: "/images/projects/motorcycle-trips.jpg",
+  //   thumbnail_path: "/images/projects/thumbnails/motorcycle-trips.jpg",
+  //   tags: ["MongoDB, Express.js, React.js, Node.js"],
+  //   title: "Motorcycle Trips React App",
+  //   description:
+  //     "This is a full-stack node.js application, and is a work in progress. Motorcycle riders can browse through and sign up for trips with other riders. Admin users can create, update and delete trips, riders, events as they choose. Express.js used to interact with MongoDB database. React.js makes requests to API endpoints on Node.js server to render data.",
+  //   githubUrl: "https://github.com/wamidgette/MotorcycleRidersMERNApp",
+  // },
 ];
 
 export default ProjectsArray;
